3229f7ed5fc4ee310e540a58dc1212642c8ddb13
rdolgov <dolgov@wavesplatform.com>
2018-05-21 17:21:28 +0300

    rewrite code

--- main.go
+++ main.go
@@ -1,45 +1,57 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
-	"net/http"
-	"time"
-
 	"collectd.org/api"
 	"collectd.org/exec"
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
 )
 
 var myClient = &http.Client{Timeout: 10 * time.Second}
 
-func getJson(url string, target interface{}) error {
-	r, err := myClient.Get(url)
-	if err != nil {
-		return err
-	}
-	defer r.Body.Close()
-	return json.NewDecoder(r.Body).Decode(target)
-}
-
 type nodeStatus struct {
-	blockchainHeight int32
-	stateHeight      int32
-	updatedTimestamp int64
-	updatedDate      string
+	BlockchainHeight int    `json:"blockchainHeight"`
+	StateHeight      int    `json:"stateHeight"`
+	UpdatedTimestamp int    `json:"updatedTimestamp"`
+	UpdatedDate      string `json:"updatedDate"`
 }
 
 func main() {
-	nodestatus := new(nodeStatus)
-	getJson("http://127.0.0.1:6869/node/status", nodestatus)
+	url := "http://127.0.0.1:6869/node/status"
+	nodeClient := http.Client{
+		Timeout: time.Second * 2, // Maximum of 2 secs
+	}
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("User-Agent", "spacecount-tutorial")
+	res, getErr := nodeClient.Do(req)
+	if getErr != nil {
+		log.Fatal(getErr)
+	}
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+	nodestatus := nodeStatus{}
+	jsonErr := json.Unmarshal(body, &nodestatus)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
 	vl := api.ValueList{
 		Identifier: api.Identifier{
 			Host:   exec.Hostname(),
-			Plugin: "node.height",
+			Plugin: "node-height",
 			Type:   "gauge",
 		},
 		Time:     time.Now(),
 		Interval: exec.Interval(),
-		Values:   []api.Value{api.Gauge(nodestatus.blockchainHeight)},
+		Values:   []api.Value{api.Gauge(nodestatus.BlockchainHeight)},
 	}
 	exec.Putval.Write(context.Background(), &vl)
 }
